551b10c5127c41afcb0fedab6b8653b104230e47
Бочков Максим <ocmax@mail.ru>
2016-10-04 17:39:48 +0300

    Первый набросок
    Релизован поиск целого числа в целочисленном массиве, который задается
    через Random

---
+++ Kasp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kasp2
+{
+    class Program
+    {
+        private const int count=100;
+        private const int valueforsearch=15;
+        private const int maxlimit=100;
+        private const int minlimit=-10;
+        static void Main(string[] args)
+        {
+            Random random= new Random();
+            List<int> arraInts= new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+             arraInts.Add(random.Next(minlimit,maxlimit));
+             Console.Write(arraInts.Last()+" ");
+            }
+            Console.WriteLine();
+           
+            FindParis(arraInts,valueforsearch);
+            Console.ReadLine();
+        }
+
+        static void FindParis(List<int> listnumber, int val)
+        {
+            for (int i = 0; i < listnumber.Count; i++)
+            {
+                int searchvalue = -(listnumber[i]-val);
+                for (int j = i; j < listnumber.Count; j++)
+                {
+                    if (searchvalue == listnumber[j]) 
+                    Console.WriteLine("Найдена пара({0})+({1}), в сумме дает {2} ",listnumber[i],listnumber[j],val);
+
+                }
+            }
+        }
+    }
+}
